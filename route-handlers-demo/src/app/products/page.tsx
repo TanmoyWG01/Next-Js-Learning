037d5f7d1dc6f4ec3e15383da74379f743548856
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 12:33:42 +0530

    feat: add ProductsPage component with links to product details

---
+++ route-handlers-demo/src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function ProductsPage() {
+    // console.log("Products server component rendered");
+    return (
+      <div>
+        <h1>Products Page</h1>
+        <Link href="/products/1">Product 1</Link>
+        <Link href="/products/2">Product 2</Link>
+        <Link href="/products/3">Product 3</Link>
+      </div>
+    );
+  }
