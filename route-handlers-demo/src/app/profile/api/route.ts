83885458a65477a5613c226e6493bc96511638e5
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 12:05:02 +0530

    fix: correct type import and improve formatting in route handler

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,7 +1,7 @@
 import { headers, cookies } from "next/headers";
-import {type NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
-export async function GET(request: NextResponse) {  
+export async function GET(request: NextRequest) {  
 
   // const requestHeader = new Headers(request.headers);
   // console.log(requestHeader.get("Authorization"));
@@ -9,19 +9,17 @@ export async function GET(request: NextResponse) {
   const headerList = await headers(); 
   console.log(headerList.get("Authorization"));
 
-  const theme =request.cookies.get("theme");
+  const theme = request.cookies.get("theme");
   console.log("Cookie Theme:", theme);
 
   const cookieStore = await cookies();
   cookieStore.set("ResultPage", "20");
   console.log(cookieStore.get("ResultPage"));
   
-
-  return new Response("<h1>Hello, Next.js Route Handlers API!</h1>",{
+  return new Response("<h1>Hello, Next.js Route Handlers API!</h1>", {
     headers: { 
       "Content-Type": "text/html",
       "Set-Cookie": "theme=dark"
-     }
+    }
   });
 }
-
