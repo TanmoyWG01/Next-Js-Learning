d4ed9072024c2e1d050267275dcfc5322306b6c3
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 13:01:01 +0530

    feat: add generateStaticParams function to define product IDs for static generation

--- route-handlers-demo/src/app/products/[id]/page.tsx
+++ route-handlers-demo/src/app/products/[id]/page.tsx
@@ -1,4 +1,12 @@
-    export default async function ProductsPage({
+export async function generateStaticParams() {
+        return [
+            { id: '1' },
+            { id: '2' },
+            { id: '3' },
+        ];
+    }
+    
+export default async function ProductsPage({
     params,
   }: {
     params: Promise<{ id: string }>;
