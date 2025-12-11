ad67e2fb92d9ed9d605932a03d84579898236713
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-11 12:20:03 +0530

    fix: update AboutPage to use async function and retrieve theme from cookies

--- route-handlers-demo/src/app/about/page.tsx
+++ route-handlers-demo/src/app/about/page.tsx
@@ -1,7 +1,11 @@
 // import { useState } from "react";
+import { cookies } from "next/headers";
 
-export default function AboutPage() {
+export default async function AboutPage() {
     // const [name, setName] = useState("");
+    const cookieStore = await cookies();
+    const theme = cookieStore.get("theme")
+    console.log(theme);
     console.log("About server component rendered");
     return <h1>About Page</h1>
   }
