2efc363f3fd0129d53b347ef6344dc430623d8e1
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-15 11:24:45 +0530

    feat: enhance ServerComponentOne to include ClientComponentOne and ServerComponentTwo

--- route-handlers-demo/src/components/server-component-one.tsx
+++ route-handlers-demo/src/components/server-component-one.tsx
@@ -1,11 +1,18 @@
 import fs from "fs";
+import { ServerComponentTwo } from "./server-component-two";
+import { ClientComponentOne } from "./client-component-one";
 
 export const ServerComponentOne = () => {
     console.log("Server Component One rendered");
     fs.readFileSync("src/components/server-component-one.tsx", "utf-8");
     return (
+        <>
         <div>
             <h3>This is Server Component One</h3>
+            <ClientComponentOne>
+                <ServerComponentTwo />
+            </ClientComponentOne>
         </div>
+        </>
     );
 }
