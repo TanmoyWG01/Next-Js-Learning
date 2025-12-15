bc6b091ad7731d3b66987d2f85fcedc2ee991aee
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-15 13:24:40 +0530

    feat: add LoadingPage component for user loading indication

---
+++ data-fetching-demo/src/app/users-server/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LoadingPage() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+        <div>Loading users...</div>
+        </div>
+    );
+} 
