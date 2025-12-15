cde06a22aff649b9c0e878d4bbfc00bf8c18c54f
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-15 11:24:15 +0530

    feat: refactor InterleavingComponent to use Client and Server components

--- route-handlers-demo/src/app/interleaving/page.tsx
+++ route-handlers-demo/src/app/interleaving/page.tsx
@@ -1,8 +1,13 @@
-export const InterleavingComponent = () => {
-    console.log("Interleaving Component rendered");
-    return (
-        <div>
-            <h3>This is Interleaving Component</h3>
-        </div>
-    );
+import { ClientComponentOne } from "@/components/client-component-one";
+import { ServerComponentOne } from "@/components/server-component-one";
+
+export default function InterleavingPage() {
+  console.log("Interleaving Component rendered");
+  return (
+    <div>
+      <h3>This is Interleaving Component</h3>
+      <ClientComponentOne />
+      <ServerComponentOne />
+    </div>
+  );
 }
