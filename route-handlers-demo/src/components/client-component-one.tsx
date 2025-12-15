3f283b742bdbbab220bdd8e9487f8e278d46d59c
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-15 11:24:23 +0530

    feat: update ClientComponentOne to accept children as props

--- route-handlers-demo/src/components/client-component-one.tsx
+++ route-handlers-demo/src/components/client-component-one.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import {useState} from "react";
+// import { ClientComponentTwo } from "./client-component-two";
+import { ServerComponentOne } from "./server-component-one";
 
-export const ClientComponentOne = () => {
+export const ClientComponentOne = ({children}: {
+    children?: React.ReactNode;
+}) => {
     const [name, setName] = useState("Batman");
     console.log("Client Component One rendered");
     return (
         <div>
             <h3>Client Component One</h3>
+            {children}
+            {/* <ClientComponentTwo/> */}
+            {/* <ServerComponentOne /> */}
         </div>
     );
 }
