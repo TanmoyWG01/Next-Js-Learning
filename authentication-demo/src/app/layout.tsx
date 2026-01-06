d65d1af1e9f55756e29cda9513082b960ec0672d
TanmoyWG01 <tanmoy.webgen@gmail.com>
2026-01-06 12:19:49 +0530

    feat: add Clerk middleware and integrate ClerkProvider in RootLayout

--- authentication-demo/src/app/layout.tsx
+++ authentication-demo/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,5 +33,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
