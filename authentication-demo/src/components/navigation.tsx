f5868d90e0f23619044a9b61518aebc7bc447457
TanmoyWG01 <tanmoy.webgen@gmail.com>
2026-01-12 18:34:39 +0530

    add: added navigation function

---
+++ authentication-demo/src/components/navigation.tsx
@@ -0,0 +1,28 @@
+import { SignInButton, SignOutButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Link from "next/link";
+
+
+export default function Navigation() {
+  return (
+    <nav className="bg-[var(--background)] border-b border-[var(--foreground)/10]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex-shrink-0">
+            <h1 className="text-xl font-semibold text-[var(--foreground)]">
+              Next.js App
+            </h1>
+          </div>
+          <div className="flex items-center gap-4">
+            <SignedOut><SignInButton mode="modal"/></SignedOut>
+            {/* <SignInButton mode="modal"/> */}
+            <SignedIn>
+            {/* <UserButton /> */}
+            <Link href="/user-profile">Profile</Link>
+            <SignOutButton />
+            </SignedIn>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
